bb2a27aa7880a01cfb4180eb25e8c0c5987e77f4
hunghx <baby10051999@gmail.com>
2024-06-11 20:53:13 +0700

    up

--- js/login.js
+++ js/login.js
@@ -1,37 +1,24 @@
-const defaultOptions = {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-};
 
-// xây dựng hàm để đăng nhâp
 
-const  handleLogin =  ()=>{
-    //  lấy ra thông tin đăng nhập
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    // call api để xác minh
-    fetch(`http://localhost:9090/api.com/v2/auth/sign-in`, {
-        method: "POST",
-        ...defaultOptions,
-        body: JSON.stringify({username, password})
-
-    })
-        .then(res => {
-            if (res.status === 200) {
-                return res.json();
-            } else{
-                throw new Error();
-            }
-            // return res.json();
+function handleLogin(){
+    //  lấy thông tin từ in put
+    let username = document.getElementById('username');
+    let password = document.getElementById('password');
+    fetch("http://localhost:8080/api.com/v1/auth/sign-in", {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      
+        //make sure to serialize your JSON body
+        body: JSON.stringify({
+         username,
+         password
         })
-        .then(res => {
-            console.log(res)
-            // lưu thông tin đăng nhập lên localstorage/ sessionstorage
-            localStorage.setItem("userLogin",JSON.stringify(res.data))
-            location.href = "../index.html"
-        })
-        .catch(error => alert("Sai tên đăng nhập hoặc mật khẩu")
-        )
+      }).then(res  =>res.json())
+      .then((data)=>{
+        console.log(data);
+      })
+      .catch(err => console.log(err))
 }
